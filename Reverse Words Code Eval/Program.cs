375bf86fb4e133db01a238cca3156b26c2bac3d4
Your Name <your@example.com>
2016-05-14 17:32:18 -0400

    ReverseWords_Initial

---
+++ Reverse Words Code Eval/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Words_Code_Eval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "Hello CodeEval";
+            string[]userLine = line.Split();
+            Array.Reverse(userLine);
+            foreach(string Words in userLine)
+            {
+                Console.Write(' ');
+                Console.Write(Words);
+            }
+            Console.WriteLine();
+            
+            
+            
+        }
+    }
+}
